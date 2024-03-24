8291d85aff176c906d8a5641598e957793449b61
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-03-24 22:15:49 +0000

    feat: implemented TeamInformation() Composable

--- app/src/main/java/com/example/premierleague/MainActivity.kt
+++ app/src/main/java/com/example/premierleague/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.premierleague
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.premierleague.ui.theme.PremierLeagueTheme
 
@@ -33,10 +38,28 @@ class MainActivity : ComponentActivity() {
 fun TeamIcon() {
 }
 
-@Composable
-fun TeamInformation(){
-}
+/**
+ * Composable that displays a team's name and ground.
+ *
+ * @param teamName is the resource ID for the string of the team's name
+ * @param teamGround is the Int that represents the team's home ground
+ * @param modifier modifiers to set to this composable
+ */
 
+@Composable
+fun TeamInformation(
+    @StringRes teamName: Int,
+    @StringRes teamGround: Int,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(teamName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+        )
+        Text(text = stringResource(R.string.stadium, teamGround))
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
